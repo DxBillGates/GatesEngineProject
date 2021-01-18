2fda2befef46f04b0dfb1b0923470a1478857600
Gates-ToMoPi <67899020+Gates-ToMoPi@users.noreply.github.com>
2021-01-18 17:17:51 +0900

    main関数を変更

--- ExeProject/main.cpp
+++ ExeProject/main.cpp
@@ -1,5 +1,14 @@
 #include "Math/GatesMath.h"
+#include<Windows.h>
+
+#ifdef _DEBUG
+#include <crtdbg.h>
 int main()
 {
-
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#else
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+#endif
+	return 0;
 }
